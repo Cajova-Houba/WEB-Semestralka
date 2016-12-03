faad23f0bc0bef33bb61ed7811dd6e9be63ea45b
Cajova-Houba <tkmushroomer@gmail.com>
2016-12-03 19:11:32 +0100

    Bootstrap added.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>KIV/WEB - semestrální práce</title>
+    <meta name="description" content="Semestrální práce z předmětu KIV/WEB">
+    <meta name="author" content="Zdenek Vales">
+  
+    <link rel="stylesheet" href="ui/css/style.css">
+	
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="ui/bootstrap/css/bootstrap.min.css">
+	
+  
+    <!--[if lt IE 9]>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <script src="ui/js/scripts.js"></script>
+	<script src="ui/bootstrap/js/bootstrap.min.js"></script>
+    <h1>Semestrální práce z KIV/WEB</h1>
+    
+    <p>Tady jednou bude semestrální práce z KIV/WEB.</p>
+    
+</body>
+</html>
